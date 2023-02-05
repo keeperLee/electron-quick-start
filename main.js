ccd67d945fc85cdadaa98ec08d633fa4f8c6cfad
keeperlee <leejamesjian@163.com>
2023-02-05 12:57:25 +0800

    feat: 主进程和渲染进程的通信

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow, ipcMain } = require("electron");
 
-app.on('ready',()=>{
-    const mainWindow = new BrowserWindow({
-        width:800,
-        height:600,
-        webPreferences:{
-            nodeIntegration:true, //允许渲染进程访问node的api
-            contextIsolation:false  //electron12后，默认是true，表示不能够访问node的api
-        }
-    })
-    mainWindow.loadFile('./index.html')
+app.on("ready", () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true, //允许渲染进程访问node的api
+      contextIsolation: false, //electron12后，默认是true，表示不能够访问node的api
+    },
+  });
+  mainWindow.loadFile("./index.html");
 
-    // const secondWindow = new BrowserWindow({
-    //     width:50,
-    //     height:50,
-    //     webPreferences:{
-    //         nodeIntegration:true
-    //     },
-    //     parent: mainWindow  //父窗口关闭，子窗口会跟着关闭
-    // })
-    // secondWindow.loadFile('./second.html')
-})
+  // const secondWindow = new BrowserWindow({
+  //     width:50,
+  //     height:50,
+  //     webPreferences:{
+  //         nodeIntegration:true
+  //     },
+  //     parent: mainWindow  //父窗口关闭，子窗口会跟着关闭
+  // })
+  // secondWindow.loadFile('./second.html')
+
+  ipcMain.on("message", (event, arg) => {
+    console.log(arg);
+    event.sender.send("reply1", "hello from main1");
+    mainWindow.webContents.send("reply2", "hello from main2");
+    mainWindow.send("reply3", "hello from main3");
+  });
+});
--- renderer.js
+++ renderer.js
@@ -5,8 +5,22 @@
  * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
  * to expose Node.js functionality from the main process.
  */
-alert(process.versions.node)
+// alert(process.versions.node)
+const {ipcRenderer} = require('electron')
 
 window.addEventListener('DOMContentLoaded',()=>{
-    alert('DOMContentLoaded')
+    ipcRenderer.send('message','hello from renderer')
+    ipcRenderer.on('reply1',(event,arg)=>{
+        document.getElementById('message1').innerHTML = arg;
+    })
+
+    ipcRenderer.on('reply2',(event,arg)=>{
+        document.getElementById('message2').innerHTML = arg;
+    })
+
+    ipcRenderer.on('reply3',(event,arg)=>{
+        document.getElementById('message3').innerHTML = arg;
+    })
 })
+
+
